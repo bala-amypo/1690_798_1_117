b80d757cd7a9814a0d17e5aa4931c94e74d8693e
bala-amypo <bala@amypo.in>
2025-12-21 15:14:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.UserAccount;
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.service.UserAccountService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService {
+
+    private final UserAccountRepository repo;
+    private final PasswordEncoder encoder;
+
+    public UserAccountServiceImpl(UserAccountRepository repo, PasswordEncoder encoder) {
+        this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    public UserAccount createUser(UserAccount user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
+
+    public UserAccount getUserById(Long id) {
+        return repo.findById(id).orElseThrow();
+    }
+
+    public UserAccount updateUserStatus(Long id, String status) {
+        UserAccount user = getUserById(id);
+        user.setStatus(status);
+        return repo.save(user);
+    }
+
+    public List<UserAccount> getAllUsers() {
+        return repo.findAll();
+    }
+
+    public Optional<UserAccount> findByUsername(String username) {
+        return repo.findByUsername(username);
+    }
+}
