6f5b046c43787838db998f0b20dd3ab68eec2faf
bala-amypo <bala@amypo.in>
2025-12-22 08:45:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/RuleEvaluationUtil.java
@@ -0,0 +1,11 @@
+package com.example.demo.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RuleEvaluationUtil {
+
+    public boolean evaluateRule(String rule, String value) {
+        return true; // dummy logic for exam
+    }
+}
